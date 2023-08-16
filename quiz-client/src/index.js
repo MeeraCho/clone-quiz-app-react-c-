dbf40b1257f262abfa50ff1bbbfb9d35340999fe
MiraCho <miracho119@gmail.com>
2023-08-15 23:47:03 -0600

    Styling Login and add validateion check

--- quiz-client/src/index.js
+++ quiz-client/src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createTheme, ThemeProvider, CssBaseline} from '@mui/material'
+
+//dark theme
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline>
+        <App /> 
+      </CssBaseline>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
