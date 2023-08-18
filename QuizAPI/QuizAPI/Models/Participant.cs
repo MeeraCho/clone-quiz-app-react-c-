3efe60f78706de84a41c3fb81714f4694a8a4788
MiraCho <miracho119@gmail.com>
2023-08-17 23:41:03 -0600

    starts Quiz API

---
+++ QuizAPI/QuizAPI/Models/Participant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuizAPI.Models
+{
+    public class Participant
+	{
+        [Key]
+        public int MyParticipantId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string? Email { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string? Name { get; set; }
+        public int Score { get; set; }
+        public int TimeTaken { get; set; }
+    }
+}
+
